17bad4a4cf695c53e81232346ac4185864ce5841
TekMal <k.kochanowska6@gmail.com>
2019-11-07 16:55:56 +0100

    cd canvas

--- apps-js-task-master-29204653c56b669acc252e60e35d7a3c9d511b79/src/index.js
+++ apps-js-task-master-29204653c56b669acc252e60e35d7a3c9d511b79/src/index.js
@@ -1,15 +1,21 @@
 const canvas = createElementWithClassname('canvas', 'canvas');
 const body = document.querySelector("body");
+canvas.setAttribute('width', '900px')
+canvas.setAttribute('height', '500px')
 body.append(canvas);
 const ctx = canvas.getContext("2d");
 function drawRect (x, y, width, height, color = 'green') {
     ctx.fillStyle = color
     ctx.fillRect(x, y, width, height)
-    ctx.strokeStyle = 'black';
+    ctx.strokeStyle = "black"
   }
-drawRect(0, 0, 30, 30)
-drawRect(60, 60, 15, 15)
 
+ctx.beginPath();
+ctx.lineWidth = "1";
+ctx.strokeStyle = "black";
+ctx.fillStyle = "green"
+ctx.fillRect(30, 30, 50, 50);
+ctx.strokeRect(30, 30, 50, 50)
 
 function createElementWithClassname(elementTag, elementClassName){
     const element = document.createElement(elementTag)
