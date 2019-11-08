b2e9f05d7f9f79fce83f85a685bb2664bf94e1a5
TekMal <k.kochanowska6@gmail.com>
2019-11-08 02:15:42 +0100

    add restart, live wip

--- apps-js-task-master-29204653c56b669acc252e60e35d7a3c9d511b79/src/index.js
+++ apps-js-task-master-29204653c56b669acc252e60e35d7a3c9d511b79/src/index.js
@@ -1,14 +1,64 @@
 const gameBoxes = document.querySelectorAll("div.gameSquare")
-//gameBoxes.forEach(item => item.addEventListener('click', makeGreenBox))
+const scorePanel = document.getElementById("scorePanel")
+const timePanel = document.getElementById("timePanel")
+const livePanel = document.getElementById("livePanel")
+const startButton  = document.getElementById("start")
+startButton.addEventListener('click', start)
+document.getElementById("reset").addEventListener('click', reset)
+let intervalBoard = null
+let intervalTime = null
+let score = 0
+let previousScore = 0
+let time = 60
+let live = 3
+
+function timer(){
+  timePanel.innerHTML =  `Czas: ${time--}`
+}
+function newScore(){
+  scorePanel.innerHTML = `Punkty: ${score}`
+}
+function newLive(liveArg){
+  livePanel.innerHTML = `Życie: ${liveArg}`
+}
+
+function clickGreenBox(){
+  createNewGameBoard()
+  score++
+  previousScore = score
+  newScore()
+}
 
 function makeGreenBox(){
-  const j = Math.floor(Math.random() * [...gameBoxes].length)
-  gameBoxes[j].style.backgroundColor = "green"
+  const i = Math.floor(Math.random() * [...gameBoxes].length)
+  gameBoxes[i].style.backgroundColor = "green"
+  gameBoxes[i].addEventListener('click', clickGreenBox)
 }
 function createNewGameBoard(){
-  gameBoxes.forEach(item => item.style.backgroundColor = "white")
-  makeGreenBox()
+  if(score !==0 && score === previousScore){
+    newLive(live--)
+  }
+  gameBoxes.forEach(item => 
+    {item.style.backgroundColor = "white"
+    item.removeEventListener('click', clickGreenBox)})
+ makeGreenBox()
 }
-setInterval(createNewGameBoard, 2000);
+function start(){
+  createNewGameBoard()
+  intervalBoard = setInterval(createNewGameBoard, 2000)
+  intervalTime = setInterval(timer, 1000)
+  startButton.removeEventListener('click', start)
+}
+function reset(){
+  score = 0
+  time = 60
+  live = 3
+  timer()
+  newScore()
+  newLive()
+  clearInterval(intervalBoard)
+  clearInterval(intervalTime)
+  createNewGameBoard()
+}
 
 //makeGreenBox()
